f09028a1d8112d5498756d7540cdfce4d3dea7b1
Ethan Van De Woestyne <ethan.van@protonmail.com>
2025-02-23 13:06:44 -0700

    Implement basic footer

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <nav className="bg-zinc-600 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-white text-base font-bold">
+            Southern Alberta Institute of Technology
+          </h1>
+          <p className="text-white">1301 16 Avenue NW | Calgary, AB T2M 0L4</p>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Footer;
